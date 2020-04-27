28364c5b7f035ffd7b9c07af5853eb55f98b7b37
minikiller <minikiller@qq.com>
2020-04-27 19:16:51 +0800

    add id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 import json
 from flask_basicauth import BasicAuth
 import random
@@ -9,6 +9,7 @@ app.config['BASIC_AUTH_PASSWORD'] = '123'
 
 basic_auth = BasicAuth(app)
 
+
 @app.route('/redfish/v1/Systems/1')
 @basic_auth.required
 def system():
@@ -16,31 +17,38 @@ def system():
         temp = json.load(f)
     return jsonify(temp)
 
+
 @app.route('/sunlf', methods=['POST'])
 @basic_auth.required
 def sunlf():
     print(request.headers)
     print(request.json)
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 class Data():
-    def __init__(self,humid,temp):
-        self.humid=humid
-        self.temp=temp
+    def __init__(self, humid, temp):
+        self.humid = humid
+        self.temp = temp
+
 
 class MyEncoder(json.JSONEncoder):
     def default(self, o):
         return o.__dict__
 
+
 @app.route('/getdata', methods=['GET'])
 def getData():
-    data=Data( random.randint(1,10), random.randint(10,20))
-    return json.dumps(data,cls=MyEncoder), 200, {'ContentType':'application/json'} 
+    data = Data(random.randint(1, 10), random.randint(10, 20))
+    return json.dumps(data, cls=MyEncoder), 200, {'ContentType': 'application/json'}
 
-@app.route('/postdata', methods=['POST'])
-def postData():
-    print("get request, json data is {}".format(request.json))
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+@app.route('/postdata/<asset_id>', methods=['POST'])
+def postData(asset_id):
+
+    print("get request, json data is {},device_id is {}".format(
+        request.json, asset_id))
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
