1d4b0dc56f9c9b0b9aa62e79e273547e4d091d97
minikiller <minikiller@qq.com>
2020-05-13 15:41:31 +0800

    change

--- api.py
+++ api.py
@@ -169,7 +169,7 @@ def login():
         token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
         ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
 
-        return jsonify({'token': token.decode('UTF-8')})
+        return jsonify({'token': token.decode('UTF-8'), 'public_id': user.public_id, 'name': user.name})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
