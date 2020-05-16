00d6c79e841f2d6852cc5f6623e369aa3724a2ff
hongyu7 <hongyu71@gmail.com>
2020-05-16 11:36:03 +0800

    .

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 basedir = os.path.abspath(os.path.dirname(__file__))
 print('base path is {}'.format(basedir))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'todo.sqlite')
+    os.path.join(basedir, 'bbwq.sqlite')
 db = SQLAlchemy(app)
 
 
