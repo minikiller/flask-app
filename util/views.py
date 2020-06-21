1e2481bdbaa6a9277b6412ee703b93e0a1b8b8e0
minikiller <minikiller@qq.com>
2020-06-21 09:37:34 +0800

    chat fixed

--- util/views.py
+++ util/views.py
@@ -15,12 +15,12 @@ from util.tts import getVoice
 def get_tts(current_user):
     data = request.get_json()
     result = getVoice(current_user.name+data["text"])
-    return jsonify({'message': "棋谱保存成功!", "url": result})
+    return jsonify({'message': "语音创建成功!", "url": result})
 
 
 @ util_api.route('/chat', methods=['POST'])
 @ token_required
 def get_chat(current_user):
     data = request.get_json()
-    result = getVoice(current_user.name+"说 "+data["text"])
-    return jsonify({'message': "棋谱保存成功!", "url": result})
+    result = getVoice(data["username"]+"说 "+data["text"])
+    return jsonify({'message': "语音创建成功!", "url": result})
